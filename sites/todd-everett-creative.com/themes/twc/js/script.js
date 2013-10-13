af148ceb35b8d162cf3d29e90b1a73c396ce6730
Jaishankar Rajagopalan <jai@rajagopalan.com>
2013-10-13 14:52:27 -0500

    Colorbox popup made 2 lines.

--- sites/todd-everett-creative.com/themes/twc/js/script.js
+++ sites/todd-everett-creative.com/themes/twc/js/script.js
@@ -3,3 +3,13 @@
  //...
 })(jQuery);
 */
+(function ($) {
+  Drupal.behaviors.addLinkToColorboxCaption =  {
+    attach: function(context, settings) {
+      var titleArray = $('#cboxTitle').text().split("||");    
+      if (titleArray.length > 1) {
+        $('#cboxTitle').html(titleArray.join("<br />"));
+      }
+    }
+  };
+})(jQuery);
